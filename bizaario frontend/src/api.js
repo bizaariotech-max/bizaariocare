307d0add30ea9da0c6dbad686ddc220a59df6936
digvijay315 <143862901+digvijay315@users.noreply.github.com>
2025-09-03 18:45:42 +0530

    update latest code

--- bizaario frontend/src/api.js
+++ bizaario frontend/src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 const instance=axios.create({
    
-          // baseURL:'https://api.bizaariocare.com/'
+        //   baseURL:'https://api.bizaariocare.com/'
           // baseURL:'http://localhost:5000/'
           baseURL:"https://bizaariocare-backend.onrender.com/"
+            // baseURL:"https://bizaariocare-backend.onrender.com/"
 
        
 })
